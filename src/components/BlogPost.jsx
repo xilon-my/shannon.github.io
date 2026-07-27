bde5bce91243c68ec61a719dd9d3135629356e19
xilon-my <130048294+xilon-my@users.noreply.github.com>
2026-07-27 16:07:34 +0800

    Add syntax highlighting with Catppuccin colors via rehype-highlight

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from 'react-router-dom'
 import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
 import './Blog.css'
 
 const postModules = import.meta.glob('../posts/*.md', { query: '?raw', import: 'default', eager: true })
@@ -73,7 +74,7 @@ export default function BlogPost() {
         </div>
       </header>
       <div className="blog-post-content">
-        <Markdown>{post.content}</Markdown>
+        <Markdown rehypePlugins={[rehypeHighlight]}>{post.content}</Markdown>
       </div>
     </article>
   )
--- src/pages/DiscoverDetail.jsx
+++ src/pages/DiscoverDetail.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from 'react-router-dom'
 import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
 import Terminal from '../components/Terminal.jsx'
 import projects from '../data/discover-projects.js'
 import './Discover.css'
@@ -72,7 +73,7 @@ export default function DiscoverDetail() {
             <p className="discover-detail-desc">{project.description}</p>
 
             <div className="discover-detail-content">
-              <Markdown>{project.detail}</Markdown>
+              <Markdown rehypePlugins={[rehypeHighlight]}>{project.detail}</Markdown>
             </div>
 
             {project.takeaway && (
